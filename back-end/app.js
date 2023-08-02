c162e476774b4505fffeecec5ceb7f211764e0b5
Carl1131 <carlsantos31@yahoo.com>
2023-08-03 08:38:21 +1200

    Finished front-end and linked with back-end

--- back-end/app.js
+++ back-end/app.js
@@ -3,9 +3,18 @@
 const express = require('express');
 const app = express();
 const connectDB = require("./config/db.js");
+const cors = require('cors');
 
 // Connect Database
 connectDB();
+
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false })); //  allows Express to read data sent using a POST or PUT request. It is used for recognizing incoming objects as JSON objects.
+
 
 
 app.get('/', (req, res) => res.send('Hello world!'));
@@ -18,6 +27,8 @@ app.use('/api/books', booksRoutes);
 
 
 
-const port = process.env.PORT || 3000;
+
+
+const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
