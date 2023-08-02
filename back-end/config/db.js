122921b9f523a7771bff03dae5177a254f761004
Carl1131 <carlsantos31@yahoo.com>
2023-08-03 10:06:21 +1200

    Hard coded URI

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -2,7 +2,8 @@
 
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoURI");
+//const db = config.get("mongoURI");
+const db = "mongodb+srv://carl31:cise_2023@cluster0.fuvrr2z.mongodb.net/";
 
 const connectDB = async () => {
   try {
