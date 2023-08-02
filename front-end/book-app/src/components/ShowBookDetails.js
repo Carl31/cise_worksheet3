42a4a1d2efc829614de5153ffc5f6e6cf9a7fcfe
Carl1131 <carlsantos31@yahoo.com>
2023-08-03 10:09:58 +1200

    Edited links for vercel deployment

--- front-end/book-app/src/components/ShowBookDetails.js
+++ front-end/book-app/src/components/ShowBookDetails.js
@@ -11,7 +11,7 @@ function ShowBookDetails(props) {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:8082/api/books/${id}`)
+      .get(`http://cise-worksheet3-backend.vercel.app/api/books/${id}`)
       .then((res) => {
         setBook(res.data);
       })
